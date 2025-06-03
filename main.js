2146b72c1713c8a04095ccce2c1ebd95c301f502
Shubham Kharade <shubham.kharade28@gmail.com>
2025-06-03 12:49:01 +0530

    Folder structure setup

---
+++ main.js
@@ -0,0 +1,15 @@
+
+/* 
+
+    1. Execute inventor/InventorExporterAlgo.exe 
+        - This stores the assembly_data.json (components, constraints, joints, etc).
+
+    2. Execute model_derivative_api/main.py
+        - This stores object_hierarchy.json and properties.json in outputs/
+
+    3. Develop the algorithm or method that uses inputs/json and produces animation sequence
+
+    4. Store animation sequence (for now) 
+        - Later it will be sent to client via nodejs server
+        
+*/
