db7fdbfaf428d6dca6ae84c424bcaefc2cde791a
Shubham Kharade <shubham.kharade28@gmail.com>
2025-06-04 17:52:44 +0530

    not much changes

--- generate_animation.js
+++ generate_animation.js
@@ -5,17 +5,18 @@ const animationOutputPath = path.join(__dirname, 'outputs', 'animation_sequence.
 
 async function generateAnimationSequence() {
     // Load JSON files at runtime
-    const objectHierarchy = JSON.parse(fs.readFileSync(path.join(__dirname, 'outputs', 'object_hierarchy.json'), 'utf-8'));
-    const properties = JSON.parse(fs.readFileSync(path.join(__dirname, 'outputs', 'properties.json'), 'utf-8'));
-    const assemblyData = JSON.parse(fs.readFileSync(path.join(__dirname, 'outputs', 'assembly_data.json'), 'utf-8'));
+    const objectHierarchy = JSON.parse(fs.readFileSync(path.join(__dirname, 'inputs', 'object_hierarchy.json'), 'utf-8'));
+    const properties = JSON.parse(fs.readFileSync(path.join(__dirname, 'inputs', 'properties.json'), 'utf-8'));
+    const assemblyData = JSON.parse(fs.readFileSync(path.join(__dirname, 'inputs', 'assembly_data.json'), 'utf-8'));
 
     console.log("Inputs we have for the animation sequence generation:");
     console.log("Object Hierarchy:", objectHierarchy);
     console.log("Properties:", properties);
     console.log("Assembly Data:", assemblyData);
 
-
     console.log("Generating animation sequence...");
+
+    // Example animation sequence generation logic 
 }
 
 module.exports = {
